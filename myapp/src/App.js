c85b5eefed55826a4e1ff5db3bd6dbf46aea7b73
mishrimalaviya <mishri1411804@gmail.com>
2024-11-15 16:10:12 +0530

    add

---
+++ myapp/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import img1 from './images/a7e0cd67345093.5b361b955c1b4.gif'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 id='fv'>WELCOME TO ARTIFICIAL INTELLIGENCE(AI)</h1>
+     <img src={img1} id="cv"></img>
+        <p>
+          Artificial intelligence (AI)is a set of technologies that allow computer to <br></br>perform tasks that mimic cognitive function
+        </p>
+        <p id="xc"><i>Mishti Malaviya</i></p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
